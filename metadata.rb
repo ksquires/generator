80f1409175ab3f73f29b22eaa12e95f32fd121fa
Kent Squires <ksquires@gmail.com>
2018-01-08 20:40:42 -0600

    initial

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+name             'generator'
+maintainer       'Kenton Squires'
+maintainer_email 'ksquires@gmail.com'
+license          'Apache-2.0'
+description      'Generates Chef code for Chef DK'
+version          '0.1.0'
+chef_version     '>= 12.0'
